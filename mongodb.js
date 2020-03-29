08abf95199f1c38e2654759371aea140faf11078
BipinKalra <bipinkalra@gmail.com>
2020-03-29 19:23:21 +0530

    Create and Read

--- mongodb.js
+++ mongodb.js
@@ -1,21 +1,6 @@
 // CRUD
 
-// const mongodb = require("mongodb")
-// const MongoClient = mongodb.MongoClient
-// const objectID = mongodb.ObjectID
-
-// We can use destructuring here
-
 const { MongoClient, ObjectID} = require("mongodb")
-
-// ObjectID that mongo uses can be generated on our own command
-// const id = new ObjectID()
-// console.log(id)
-// console.log(id.getTimestamp())
-
-// Using binary instead of string for ID reduces the length by half
-// console.log(id.id.length)
-// console.log(id.toHexString().length)
 
 // Complete IP is being used here instead of just typing localhost as using localhost
 // was bringing about some weird slowdown issues
@@ -30,8 +15,6 @@ MongoClient.connect(connectionURL, {
     return console.log("Unable to connect to database!")
   }
 
-  // console.log("Connected to DB!")
-
   // Takes in just the database name and returns a db reference after creating it for us
   const db = client.db(databaseName)
 
@@ -39,61 +22,37 @@ MongoClient.connect(connectionURL, {
   // tables - connections
   // records - documents
 
-  // Inserting one into DB without any callback  
-  // db.collection("users").insertOne({
-  //   name: "Bipin",
-  //   age: 22
-  // })
+  // Searching for a user using one or more fields as a criteria
+  // If no user is found matching given credentials, null would be returned instead of an error
+  db.collection("users").findOne({
+    _id: new ObjectID("5e8098ce5f23fb7a06c2d1e0")
+    // name: "Himanshi",
+    // age: 1
+  }, (error, user) => {
+    if (error) {
+      return console.log("Couldn't fetch user!")
+    }
 
-//   // Inserting along with callback
-  // db.collection("users").insertOne({
-  //   // _id: id,
-  //   name: "Suresh",
-  //   age: 25
-  // }, (error, result) => {
-  //   if (error) {
-  //     return console.log("Unable to insert user!")
-  //   }
+    console.log(user)
+  })
 
-  //   // Array of all documents
-  //   console.log(result.ops)
-  // })
+  // Finding multiple documents
+  // We get a cursor when using find which is a pointer to data and can be used to work extensively with it
+  db.collection("users").find({
+    name: "Bipin"
+  }).toArray((error, users) => {
+    console.log(users)
+  })
 
-//   db.collection("users").insertMany([
-//     {
-//       name: "Himanshi",
-//       age: 21
-//     },
-//     {
-//       name: "Honey Singh",
-//       age: 100
-//     }
-//   ], (error, result) => {
-//     if (error) {
-//       return console.log("Unable to insert users!")
-//     }
+  db.collection("users").find({
+    name: "Bipin"
+  }).count((error, count) => {
+    console.log(count)
+  })
 
-//     console.log(result.ops)
-//   })
-
-  // db.collection("tasks").insertMany([
-  //   {
-  //     description: "First Task",
-  //     completed: true
-  //   },
-  //   {
-  //     description: "Second Task",
-  //     completed: false
-  //   },
-  //   {
-  //     description: "Task three",
-  //     completed: false
-  //   }
-  // ], (error, result) => {
-  //   if (error) {
-  //     return console.log("Unable to insert tasks!")
-  //   }
-
-  //   console.log(result.ops)
-  // })
+  db.collection("tasks").find({
+    completed: false
+  }).toArray((error, tasks) => {
+    console.log(tasks)
+  })
 })
