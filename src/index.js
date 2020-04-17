714a21005f74d65b5fd054dd38bbc0c9165d6001
BipinKalra <bipinkalra@gmail.com>
2020-04-18 02:13:24 +0530

    multer integration

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const taskRouter = require("./routers/task")
 const app = express()
 const port = process.env.PORT || 9000
 
+
+const multer = require("multer")
+const upload = multer({
+  dest: "images"
+})
+// Here upload.single("upload") looks for an entity titled "upload in the form data"
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send()
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
