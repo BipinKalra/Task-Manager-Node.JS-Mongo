9282ff6c4352901329cf7f3c11e8b1d07a8b0b8a
BipinKalra <bipinkalra@gmail.com>
2020-05-26 17:44:36 +0530

    setting up testing

---
+++ tests/math.test.js
@@ -0,0 +1,13 @@
+// Here the ".test" extension before ".js" lets jest know that this is a test file
+
+
+
+
+// An empty test case like so would pass as it doesn't throw any error
+// test("Hello World!", () => {
+// })
+
+// Following test case is going to fail.
+// test("This should fail!", () => {
+//   throw new Error("Failure!")
+// })
